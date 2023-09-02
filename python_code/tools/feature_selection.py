b1476e101887de2fa6c0f5382e8ef4aea248ed0e
Wilder González Díaz <wildergd@gmail.com>
2023-09-02 14:34:48 -0600

    fix: add max-features param to feature-selection

--- python_code/tools/feature_selection.py
+++ python_code/tools/feature_selection.py
@@ -23,16 +23,19 @@ warnings.filterwarnings('ignore')
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 parser.add_argument("-f", "--file", help="Path to dataset file (CSV format)")
 parser.add_argument("-m", "--method", help="ML method. One of: rlo, ranforest, svm.")
+parser.add_argument("-mf", "--max-features", default="20", help="Number of features to be selected. Defaults to 20")
 args = vars(parser.parse_args())
 
 # Set up parameters
 csv_file = args["file"]
 classification_method = args["method"]
+max_feaures = int(args["max_features"]) if args["max_features"].isdigit() else 20
 seed = 90
 
 def get_estimator(
     method: str,
-    random_state: int = None
+    random_state: int = None,
+    max_features: int = 20
 ):
     if method == 'rlo':
         return LogisticRegressionCV(
@@ -44,7 +47,7 @@ def get_estimator(
     if method == 'rf':
         return RandomForestClassifier(
             bootstrap = False,
-            max_features = 10,
+            max_features = max_feaures,
             n_jobs = -1,
             random_state = random_state
         )
@@ -85,7 +88,7 @@ if __name__ == '__main__':
         cv = 5,
         verbose = 1,
         scoring = "roc_auc",
-        max_features = 5,
+        max_features = max_feaures,
         crossover_proba = 0.5,
         mutation_proba = 0.2,
         crossover_independent_proba = 0.5,
@@ -98,7 +101,7 @@ if __name__ == '__main__':
 
     # datasets path
     DATASETS_PATH = path.realpath(path.join(SCRIPT_DIR, '..', '..', 'dataset'))
-    output_folder = f'{DATASETS_PATH}/features/{classification_method}'
+    output_folder = f'{DATASETS_PATH}/features/{max_feaures}-features/{classification_method}'
     file_name, _ = path.splitext(path.basename(path.abspath(csv_file)))
     
     if not path.exists(output_folder):
